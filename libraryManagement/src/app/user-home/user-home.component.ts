aa729ca823f7b6044d05b83c2b4c7a43a6161b2d
Amol Wasu <wasuamol@gmail.com>
2020-06-07 16:02:52 +0530

    ghghgh

--- libraryManagement/src/app/user-home/user-home.component.ts
+++ libraryManagement/src/app/user-home/user-home.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormControl , FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+
+import { UserAuthServiceService } from '../services/user-auth-service.service';
+import { User } from '../model/user';
 
 @Component({
   selector: 'app-user-home',
@@ -7,9 +13,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserHomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userAuthService : UserAuthServiceService, private router : Router) { } 
 
   ngOnInit() {
+    if((this.userAuthService.isLoggedIn()) )  
+    {  
+        let typ = localStorage.getItem('type');  
+        if(typ=='L'){
+          this.router.navigate(['/adminhome']);  
+        }else{
+          this.router.navigate(['/userhome']);  
+        }
+    }  
+    else  
+    {  
+        this.router.navigate(['/login']);  
+    }
   }
+
+  
 
 }
